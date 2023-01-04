91ecf2eab524b3a063a967828cb2db5e3ea7e8d4
pranto paul <prantopaul55555@gmail.com>
2023-01-04 12:31:00 +0600

    Enhance todo list functionality with checkbox and styling for completed tasks

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const todos = [];
 function createTodoElm(title, des, todoId) {
   //creating todo's elements
   const li = document.createElement("li");
+  const checkbox = document.createElement("input");
+  const body = document.createElement("div");
   const h2 = document.createElement("h2");
   const p = document.createElement("p");
   const button = document.createElement("button");
@@ -11,6 +13,12 @@ function createTodoElm(title, des, todoId) {
   // adding attributes to those elements
   li.classList.add("todo");
   li.id = todoId;
+  checkbox.type = "checkbox";
+  checkbox.classList.add("todo-check");
+  checkbox.addEventListener("change", (e) => {
+    e.target.closest(".todo").classList.toggle("done", e.target.checked);
+  });
+  body.classList.add("todo-body");
   button.setAttribute("onClick", `deleteTodo(${todoId})`);
 
   //adding childs to those elements
@@ -18,9 +26,12 @@ function createTodoElm(title, des, todoId) {
   p.innerText = des;
   button.innerText = "Delete";
 
+  body.appendChild(h2);
+  body.appendChild(p);
+
   //appneding childs of li
-  li.appendChild(h2);
-  li.appendChild(p);
+  li.appendChild(checkbox);
+  li.appendChild(body);
   li.appendChild(button);
 
   return li;
